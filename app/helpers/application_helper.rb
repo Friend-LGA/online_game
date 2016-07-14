8a59b0768649e3aeb2f631e9e650148feed44f09
Roma Nuyskov <gm.wroman@gmail.com>
2016-07-14 11:17:53 +0300

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when 'notice'
+        'alert alert-info'
+      when 'alert'
+        'alert alert-error'
+    end
+  end
 end
