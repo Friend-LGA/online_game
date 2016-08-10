ff5bf0f7c90fc79eae229916d199124cfcb02aec
Artyom Alimov <alimchik959@mail.ru>
2016-08-10 15:53:12 +0300

    Add errors locales

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -2,4 +2,13 @@
 class GameSession < ApplicationRecord
   belongs_to :white_user, class_name: 'User'
   belongs_to :black_user, class_name: 'User'
+
+  validates :white_user, :black_user, presence: true
+  validate :matches?
+
+  def matches?
+    return if white_user_id != black_user_id
+
+    errors[:base] << 'Пользователи не могут совпадать'
+  end
 end
