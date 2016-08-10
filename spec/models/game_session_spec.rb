d93673f2ad1f2bb8aa2602b93807fd9fde53fd53
Artyom Alimov <alimchik959@mail.ru>
2016-08-10 15:53:12 +0300

    Add errors locales

--- spec/models/game_session_spec.rb
+++ spec/models/game_session_spec.rb
@@ -2,5 +2,16 @@
 require 'rails_helper'
 
 RSpec.describe GameSession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:black_user) }
+    it { is_expected.to validate_presence_of(:white_user) }
+
+    context 'when users have the same id' do
+      let(:subject_invalid) { GameSession.create(white_user_id: 123, black_user_id: 123) }
+
+      it 'should be invalid' do
+        expect(subject_invalid).to be_invalid
+      end
+    end
+  end
 end
